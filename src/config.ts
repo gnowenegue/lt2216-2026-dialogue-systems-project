3bbc6a20d2c5e9f9cf7a5de3db08dfae1c4d790a
Eugene Wong <gnow.enegue@gmail.com>
2026-03-18 18:31:15 +0100

    Integrate with Custom Speech for better recognition of the category words.

--- src/config.ts
+++ src/config.ts
@@ -12,8 +12,8 @@ const azureLanguageCredentials = {
   endpoint:
     "https://lt2216.cognitiveservices.azure.com/language/:analyze-conversations?api-version=2024-11-15-preview",
   key: NLU_KEY,
-  deploymentName: "project",
-  projectName: "project",
+  deploymentName: "Project",
+  projectName: "Project",
 };
 
 export const settings: Settings = {
@@ -24,6 +24,7 @@ export const settings: Settings = {
   asrDefaultNoInputTimeout: 5000,
   locale: "en-US",
   ttsDefaultVoice: "en-US-DavisNeural",
+  speechRecognitionEndpointId: "2b4b8761-8d0e-44ce-89a5-15ca6b7cd335",
 };
 
 export const speechConfig = sdk.SpeechConfig.fromSubscription(KEY, "swedencentral");
