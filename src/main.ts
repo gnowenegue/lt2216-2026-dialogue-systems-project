32cf385d70170e69ced515980770750954734bc1
Eugene Wong <gnow.enegue@gmail.com>
2026-03-19 23:44:29 +0100

    Implement text input to ask questions.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createBrowserInspector } from "@statelyai/inspect";
 import { createActor } from "xstate";
 import { dmMachine } from "./dm";
 import "./style.css";
+import type { DMEvents } from "./types";
 
 document.getElementById("cover")?.remove();
 
@@ -27,24 +28,59 @@ dmActor.subscribe((snapshot) => {
   console.groupEnd();
 });
 
-const gameButton = document.getElementById("game");
-const skipButton = document.getElementById("skip");
+const gameButton = document.getElementById("game") as HTMLButtonElement;
+const skipButton = document.getElementById("skip") as HTMLButtonElement;
 
-const speechStateMeta = document.getElementById("spst-meta");
-const speechStateMetaValue = speechStateMeta?.querySelector("span");
+const speechStateMeta = document.getElementById(
+  "spst-meta",
+) as HTMLParagraphElement;
+const speechStateMetaValue =
+  speechStateMeta.querySelector<HTMLSpanElement>("span");
 
-const questionsRemaining = document.getElementById("questions-remaining");
-const questionsRemainingValue = questionsRemaining?.querySelector("span");
+const questionsRemaining = document.getElementById(
+  "questions-remaining",
+) as HTMLParagraphElement;
+const questionsRemainingValue =
+  questionsRemaining.querySelector<HTMLSpanElement>("span");
 
-const rules = document.getElementById("rules");
+const rules = document.getElementById("rules") as HTMLDivElement;
 
-gameButton?.addEventListener("click", () => {
+const textFallbackForm = document.getElementById(
+  "text-fallback-form",
+) as HTMLFormElement;
+const textInput = document.getElementById("text-input") as HTMLInputElement;
+const textButton = document.getElementById("text-submit") as HTMLButtonElement;
+
+textFallbackForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const text = textInput.value.trim();
+  if (text) {
+    dmActor.send({
+      type: "RECOGNISED",
+      value: [{ utterance: text }],
+      nluValue: null,
+    } as DMEvents);
+
+    const spstRef = dmActor.getSnapshot().context.spstRef;
+    const asrRef = spstRef.getSnapshot().context.asrRef;
+    if (asrRef) {
+      asrRef.send({ type: "STOP" });
+    } else {
+      dmActor.send({ type: "LISTEN_COMPLETE" });
+    }
+
+    textInput.value = "";
+  }
+});
+
+gameButton.addEventListener("click", () => {
   dmActor.send({ type: "CLICK" });
 
   gameButton.classList.add("hidden");
 });
 
-skipButton?.addEventListener("click", () => {
+skipButton.addEventListener("click", () => {
   dmActor.send({ type: "SPEAK_COMPLETE" });
 });
 
@@ -73,16 +109,19 @@ dmActor.subscribe((snapshot) => {
     idle: "Off",
   };
 
+  const isRecognising = metaView === "recognising";
+
   if (metaView && statusMessages[metaView] && speechStateMetaValue) {
     speechStateMetaValue.innerText = statusMessages[metaView];
-    speechStateMetaValue.classList.toggle(
-      "text-green-500",
-      metaView === "recognising",
-    );
-    speechStateMetaValue.classList.toggle(
-      "text-red-500",
-      metaView !== "recognising",
-    );
+    speechStateMetaValue.classList.toggle("text-green-500", isRecognising);
+    speechStateMetaValue.classList.toggle("text-red-500", !isRecognising);
+  }
+
+  textInput.disabled = !isRecognising;
+  textButton.disabled = !isRecognising;
+
+  if (!textInput.disabled && isRecognising) {
+    textInput.focus();
   }
 
   // 3. Update Questions Counter
@@ -96,9 +135,12 @@ dmActor.subscribe((snapshot) => {
     "hidden",
     !(snapshot.matches("Greeting") || snapshot.matches("Game")),
   );
-  skipButton?.classList.toggle("hidden", !snapshot.matches({ Greeting: "Prompt" }));
+  skipButton.classList.toggle(
+    "hidden",
+    !snapshot.matches({ Greeting: "Prompt" }),
+  );
 
   if (snapshot.matches("Done")) {
-    gameButton?.classList.remove("hidden");
+    gameButton.classList.remove("hidden");
   }
 });
